d06ac9c46562999945ee52630db762983061f436
Sonnic3050 <94940550+Sonnic3050@users.noreply.github.com>
2022-05-22 19:54:00 -0500

    Initial commit

---
+++ template-parts/footer-styles.php
@@ -0,0 +1,17 @@
+<!-- footer styles -->
+
+<style> .u-footer {
+  background-image: none;
+}
+.u-footer .u-sheet-1 {
+  min-height: 120px;
+}
+.u-footer .u-text-1 {
+  width: 417px;
+  margin: 49px auto 60px;
+}
+@media (max-width: 575px) {
+  .u-footer .u-text-1 {
+    width: 340px;
+  }
+}</style>
